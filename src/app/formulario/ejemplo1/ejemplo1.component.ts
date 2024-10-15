a7046186d495019fee39ab86134881e4392807ec
Ziyatsi Fanye Rocha Guerrero <ziyatsir@gmail.com>
2024-10-14 20:59:34 -0600

    segundo commit 14 oct RUTAS

--- src/app/formulario/ejemplo1/ejemplo1.component.ts
+++ src/app/formulario/ejemplo1/ejemplo1.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder,FormGroup,  ReactiveFormsModule } from '@angular/forms';
 
+interface Usuario{
+  nombre: string;
+  edad: number;
+  email:string;
+}
+
+
+
+
+
+
 @Component({
   selector: 'app-ejemplo1',
   standalone: true,
@@ -8,24 +19,61 @@ import { FormBuilder,FormGroup,  ReactiveFormsModule } from '@angular/forms';
   templateUrl: './ejemplo1.component.html',
   styles: ``
 })
-export class Ejemplo1Component implements OnInit{
+export /*default*/ class Ejemplo1Component implements OnInit{
 
 
   formGroup!: FormGroup;
-  constructor(private readonly fb: FormBuilder){}
 
+ nombre:string="ziyatsi"
+
+
+ persona:Usuario={
+  nombre:'',
+  edad: 0,
+  email:''
+ }
+
+
+
+
+
+
+
+  constructor(private readonly fb: FormBuilder){}
 
   ngOnInit(): void {
     this.formGroup = this.initForm();
   }
 initForm():FormGroup{
   return this.fb.group({
-    email:[''],
-    password:['']
+    nombre:[''],
+    edad:[''],
+    email:['']
   })
 }
-ngSubmit():void{
-  console.log('Form ->', this.formGroup.value);
+
+
+onSubmit():void{
+  const{nombre,edad,email} = this.formGroup.value;
+  this.persona.nombre=nombre
+  this.persona.edad=edad
+  this.persona.email=email
+  //this.alumnos.push(this.persona)
+let personaJSON = JSON.stringify(this.persona);
+  
+
+  //console.log('Form ->', this.formGroup.value);
+  //localStorage.setItem("nombre",this.nombre); //aqui se almacena donde dice ziyatsi
+  localStorage.setItem("persona",personaJSON);
+}
+
+subImprime():void{
+  const usuarioGuardado = localStorage.getItem('persona');
+  if(usuarioGuardado){
+    const usuarioRecuperado: Usuario = JSON.parse(usuarioGuardado);
+    this.persona=usuarioRecuperado;
+  }
 }
 
 }
+
