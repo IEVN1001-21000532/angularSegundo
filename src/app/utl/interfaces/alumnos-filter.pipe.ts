a8a51338751321bcbc11767990cc6dee9ce751c5
Ziyatsi Fanye Rocha Guerrero <ziyatsir@gmail.com>
2024-11-14 21:02:58 -0600

    servicios final alumnos 14/11/24

--- src/app/utl/interfaces/alumnos-filter.pipe.ts
+++ src/app/utl/interfaces/alumnos-filter.pipe.ts
@@ -12,3 +12,4 @@ export class AlumnosFilterPipe implements PipeTransform {
   }
 
 }
+ 
--- src/app/utl/utl.routes.ts
+++ src/app/utl/utl.routes.ts
@@ -9,5 +9,15 @@ export default[
         path: 'agregar',
         loadComponent:()=>import('./agregar/agregar.component')
 
+    },
+    {
+        path: 'eliminar/:matricula',
+        loadComponent:()=>import('./eliminar/eliminar.component')
+
+    },
+    {
+        path: 'editar/:matricula',
+        loadComponent:()=>import('./editar/editar.component')
+
     }
 ] as Routes
